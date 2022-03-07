4a73afdc168afc53e9524b0f712fb6b879636f37
Eli Gundry <eligundry@gmail.com>
2022-03-07 11:05:37 -0500

    Switch arrowParens to always and document

--- prettier/index.js
+++ prettier/index.js
@@ -2,9 +2,15 @@
  * @type {import('prettier').Config}
  */
 module.exports = {
-  arrowParens: 'avoid',
+  // Prettier 2 default. Can lead to smaller diffs.
+  arrowParens: 'always',
+  // Javascript only needs semicolons in unique cirumstances.
   semi: false,
+  // Easier to type
   singleQuote: true,
+  // Most popular libraries use 2 space tab stops.
   tabWidth: 2,
+  // Smaller diffs when adding to objects and arrays. No trailing commas in
+  // function parameters.
   trailingComma: 'es5',
 }
